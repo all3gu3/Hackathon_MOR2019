db72100933f6205f5ed55b99d8a63b6b94ea52de
Leoperez3006 <leoperez3006@gmail.com>
2019-10-19 14:51:35 -0500

    Se actualizó header.php

--- original/header.php
+++ original/header.php
@@ -71,8 +71,6 @@
                                     <li><a href="index.php">Home</a></li>
                                     
                                     <li><a href="about-us.php">About Us</a></li> 
-
-                                    <li><a href="contact.php">Contact</a></li>
                                 </ul>
 
                                 <!-- Search Form  -->
